b919d5b0db0e3c2bcc6af302412cfe0dac87c303
kenan <houkenan0620@126.com>
2017-02-14 15:48:11 +0800

    170214、初始化项目

---
+++ ConanVideoRecord/ConanVideoRecord/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ConanVideoRecord
+//
+//  Created by Conan on 2017/2/14.
+//  Copyright © 2017年 Conan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ConanVideoRecord/ConanVideoRecord/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ConanVideoRecord
+//
+//  Created by Conan on 2017/2/14.
+//  Copyright © 2017年 Conan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
